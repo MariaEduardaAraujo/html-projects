eef5ecbdb4235e66cde4c011e8a3ee17f380a58e
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-04-25 22:04:33 -0300

    Add files via upload

---
+++ Atividades/TesteAula/processa.php
@@ -0,0 +1,6 @@
+<? php
+	$email = addslashes($_POST['email'])
+	$confirmaremail = addslashes($_POST['confirmarEmail'])
+	$senha = addslashes($_POST['senha'])
+	$estado = addslashes($_POST['estado'])
+?>
