cd43d4fc32f6d296d61b9fd7323821fac1fc872b
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-06-16 12:56:30 -0300

    fix: removendo import de regex que foi adicionado por engano

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,38 +1,37 @@
-    import regex from 'regex'
-    import bcrypt from 'bcrypt'
-    import { AppError } from "../utils/AppError.js"
-    import { gerarAccessToken } from '../utils/jwt.js'
+import bcrypt from 'bcrypt'
+import { AppError } from "../utils/AppError.js"
+import { gerarAccessToken } from '../utils/jwt.js'
 
-    export class AutenticacaoService{
-        constructor (usuariosRepository){
-            this.usuariosRepository = usuariosRepository
-        }
-        async criar({ nome, email, senha, papel = "OPERADOR" }){
-            if (!senha || senha.length < 8){
-                throw new AppError("Senha deve ter no mínimo 8 caracteres", 400)
-            }
-
-            const autenticacaoEmail = await this.usuariosRepository.buscarPorEmail(email)
-            if(autenticacaoEmail){
-                throw new AppError("Credenciais inválidas", 409);
-            }
-            
-            const senhaHash = await bcrypt.hash(senha, 10);
-            return this.usuariosRepository.criar({ nome, email, senhaHash, papel })
-        }
-        async login({ email, senha }) {
-            const emailUser = await this.usuariosRepository.buscarPorEmail(email)
-            if (!emailUser) throw new AppError("Credenciais inválidas", 401)
-
-            const senhaUser = await bcrypt.compare(senha, emailUser.senhaHash)
-            if (!senhaUser) throw new AppError("Credenciais inválidas", 401)
-
-            const token = gerarAccessToken({
-                id: emailUser.id,
-                nome: emailUser.nome,
-                email: emailUser.email,
-                papel: emailUser.papel
-            })
-            return { accessToken: token }
-        }
+export class AutenticacaoService{
+    constructor (usuariosRepository){
+        this.usuariosRepository = usuariosRepository
     }
+    async criar({ nome, email, senha, papel = "OPERADOR" }){
+        if (!senha || senha.length < 8){
+            throw new AppError("Senha deve ter no mínimo 8 caracteres", 400)
+        }
+
+        const autenticacaoEmail = await this.usuariosRepository.buscarPorEmail(email)
+        if(autenticacaoEmail){
+            throw new AppError("Credenciais inválidas", 409);
+        }
+        
+        const senhaHash = await bcrypt.hash(senha, 10);
+        return this.usuariosRepository.criar({ nome, email, senhaHash, papel })
+    }
+    async login({ email, senha }) {
+        const emailUser = await this.usuariosRepository.buscarPorEmail(email)
+        if (!emailUser) throw new AppError("Credenciais inválidas", 401)
+
+        const senhaUser = await bcrypt.compare(senha, emailUser.senhaHash)
+        if (!senhaUser) throw new AppError("Credenciais inválidas", 401)
+
+        const token = gerarAccessToken({
+            id: emailUser.id,
+            nome: emailUser.nome,
+            email: emailUser.email,
+            papel: emailUser.papel
+        })
+        return { accessToken: token }
+    }
+}
